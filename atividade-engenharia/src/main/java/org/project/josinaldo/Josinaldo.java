c7297ea5b0df85fa2fab7939ae62f0e4cf82c8d3
josinaldojr <josinaldo.pontes@dcx.ufpb.br>
2022-05-24 15:31:55 -0300

    feat: create Josinaldo

---
+++ atividade-engenharia/src/main/java/org/project/josinaldo/Josinaldo.java
@@ -0,0 +1,15 @@
+package org.project.josinaldo;
+
+public class Josinaldo {
+
+    private String name;
+
+    public Josinaldo() {
+        this.name = "Josinaldo de Pontes Bezerra Júnior";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
