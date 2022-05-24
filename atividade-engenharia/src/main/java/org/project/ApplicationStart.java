bc5dace5ef0a552b88f8a25ffdf87fc218f4eb73
josinaldojr <josinaldo.pontes@dcx.ufpb.br>
2022-05-24 15:14:37 -0300

    feat: add initial project

---
+++ atividade-engenharia/src/main/java/org/project/ApplicationStart.java
@@ -0,0 +1,9 @@
+package org.project;
+
+public class ApplicationStart {
+    public static void main(String[] args) {
+        System.out.println(
+                "Quero Café"
+        );
+    }
+}
